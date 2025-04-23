27e59a714659df2feadf0752f930ab91a5d341dd
ninjabase8085 <pranjal.3vyas@gmail.com>
2025-04-23 17:15:33 +0530

    Update DeviceToken.js

--- app-pushapp/models/DeviceToken.js
+++ app-pushapp/models/DeviceToken.js
@@ -1,16 +1,21 @@
-const mongon = require("@bootloader/mongon");
+const mongon = require('@bootloader/mongon');
 
-const eventSchema = mongon.Schema(
+const deviceTokenSchema = mongon.Schema(
   {
-    user_id: { type: String, required: true },
-    event_name: { type: String, required: true },
-    attributes: { type: Map, of: String }, // Stores attributes as a key-value map
-    timestamp: { type: Date, default: Date.now },
+    device_id: { type: String, required: true, unique: true },
+    token: { type: String, required: true, unique: true },
+    platform: { type: String, required: true },
+    user_id: { type: String },
+    company_id: { type: String, required: true },
+    channel_id: { type: String, required: true },
+    session_id: { type: String, required: true }, // session ID to track sessions
+    last_active: { type: Date, default: Date.now }, // timestamp to track session activity
+    status: { type: Boolean, default: true },
   },
   {
-    collection: "EVENT",
+    collection: "DEVICE_TOKEN",
   }
 );
 
-// module.exports = mongon.model(eventSchema);
-module.exports = eventSchema;
+// module.exports = mongon.model(deviceTokenSchema);
+module.exports = deviceTokenSchema;
